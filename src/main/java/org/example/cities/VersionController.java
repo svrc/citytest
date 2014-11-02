d35047660552d348e7ae7999b167100897344fbf
Stu Charlton <stuartc@mac.com>
2014-11-02 13:39:29 -0700

    change version

--- src/main/java/org/example/cities/VersionController.java
+++ src/main/java/org/example/cities/VersionController.java
@@ -8,6 +8,6 @@ public class VersionController {
 
     @RequestMapping("/version")
     public String index() {
-        return "Greetings Paris Roadshow!";
+        return "Stu was here!";
     }
 }
